2f04d6031a7c80fbd4fc294375542b8ad6665e22
NESTOR GONCALVES DE RESENDE JUNIOR <nestor.junior@gmail.com>
2022-07-26 17:23:41 -0300

    delete "." in bash

--- 03-programas.sh
+++ 03-programas.sh
@@ -25,7 +25,7 @@ doSeparador "Instalando aws-cli..."
 
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "/tmp/awscliv2.zip" 
 unzip /tmp/awscliv2.zip -d /tmp
-sudo ./tmp/aws/install 
+sudo /tmp/aws/install 
 
 doSeparador "Instalando terraform..."
 
