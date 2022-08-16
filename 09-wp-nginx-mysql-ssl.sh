2b6650d163a868aa6c15f3492b00f840bbf8b404
Nestor Junior <nestor.junior@gmail.com>
2022-08-16 03:40:26 -0300

    add news configurations

--- 09-wp-nginx-mysql-ssl.sh
+++ 09-wp-nginx-mysql-ssl.sh
@@ -4,6 +4,9 @@
 
 name="wp-nginx-mysql-ssl"
 
+#informado o domímnio para o nginx
+sed -i "s/<<example.com>>/${gdns}/g" "./arquivos/nginx.conf"
+
 doSeparador "Instalando ${name}"
 
 sudo docker-compose -f "arquivos/${name}.yml" up -d
