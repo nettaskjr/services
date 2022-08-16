7f39bebf45576db57b57907afd6c257d60064f32
Nestor Junior <nestor.junior@gmail.com>
2022-08-16 15:18:16 -0300

    ...

--- 00-run.sh
+++ 00-run.sh
@@ -18,7 +18,7 @@ while true; do
         --nginx )               ./07-nginx.sh; shift ;;
         --vault )               ./08-vault.sh; shift ;;
         --nginx-ssl)            ./09-nginx-ssl.sh; shift ;;
-        * )                     break;; #[ -f "echo arquivos/echo "${4}" | sed 's/--//g'" ] && docker-compose -f "arquivos/${4}.yml" up -d;;
+        * )                     break ;; #[ -f "echo arquivos/echo "${4}" | sed 's/--//g'" ] && docker-compose -f "arquivos/${4}.yml" up -d;;
     esac
 done
 
