4c395dc6e63fe88506c9f7e9ae964092bb8e5eb9
Nestor Junior <nestor.junior@gmail.com>
2022-08-17 12:21:12 -0300

    alter name in deploy

--- 09-wp-sql-nginx-ssl.sh
+++ 09-wp-sql-nginx-ssl.sh
@@ -2,13 +2,14 @@
 
 . 02-funcoes.sh
 
-file="wp-sql-nginx-ssl"
+txt="wp-sql-nginx-ssl"
+file="arquivos/${txt}.yml"
 
-doSeparador "Instalando ${file}"
+doSeparador "Instalando ${txt}"
 
 # atualiza as informações para geração do certificado SSL
 sed -i "s/<<EMAIL>>/${geml}/g" "${file}"
 sed -i "s/<<HOST>>/${gdns}/g" "${file}"
 sed -i "s/<<HOST>>/${gdns}/g" "${file}"
 
-sudo docker-compose -f "arquivos/${file}.yml" up -d
+sudo docker-compose -f "${file}" up -d
