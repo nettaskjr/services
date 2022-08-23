5347b72c2defda3a8aff63312819d70a8c1fa899
Nestor Junior <nestor.junior@gmail.com>
2022-08-23 18:02:49 -0300

    ...

--- 03-docker.sh
+++ 03-docker.sh
@@ -10,3 +10,9 @@ apps="${apps} docker-compose"   # rotinas usando docker
 doApt "${apps}"
 
 sudo usermod -aG docker ${gusr} # adicionar o usuário ao grupo do docker
+
+# instalação do portainer
+
+docker container run -d \
+  -p 9000:9000 \
+  -v /var/run/docker.sock:/var/run/docker.sock portainer/portainer
