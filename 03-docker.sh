860bdbd7316b48f3af781952594a09980f5cb4f4
Nestor Junior <netor.junior@gmail.com>
2023-02-04 19:58:00 -0300

    ...

--- 03-docker.sh
+++ 03-docker.sh
@@ -4,9 +4,25 @@
 
 doSeparador "Instalando docker e docker-compose..."
 
-# [ ]nova instalação do docker a testar
-#sudo apt-get remove docker docker-engine docker.io containerd runc
+# [ ]nova instalação do docker a ser testada
 
+# remove qualquer instalação antiga do docker
+# sudo apt-get remove -y docker docker-engine docker.io containerd runc
+
+# adicinar a chave GPG oficial do Docker
+# sudo mkdir -p /etc/apt/keyrings
+# curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+
+# adicionar o repositorio do Docker
+# echo \
+#  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
+#  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+# corrigindo possivel erro no update
+# sudo chmod a+r /etc/apt/keyrings/docker.gpg
+
+# instalar o docker e docker compose
+# sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose docker-compose-plugin
 
 apps="docker.io"                # container
 apps="${apps} docker-compose"   # rotinas usando docker
