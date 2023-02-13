c1b8f9b0b6b30bd041de701d2525d2ad9bf22c59
Nestor Junior <nestor.junior@gmail.com>
2023-02-12 23:51:27 -0300

    new version of terraform (1.3.8)

--- 87-terraform.sh
+++ 87-terraform.sh
@@ -4,7 +4,7 @@
 
 doSeparador "Instalando terraform..."
 
-vs="1.3.7" 
+vs="1.3.8" 
 arquivo="terraform_${vs}_linux_amd64.zip" 
 
 wget -c -P "/tmp" "https://releases.hashicorp.com/terraform/${vs}/${arquivo}" 
