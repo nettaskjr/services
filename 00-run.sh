05d161b8eeb9f606a5cc67c2223c982ad0a099d9
Nestor Junior <nestor.junior@gmail.com>
2025-05-24 20:34:48 -0300

    ..

--- 00-run.sh
+++ 00-run.sh
@@ -52,6 +52,7 @@ while true; do
         --wp-nginx-ssl )        ./10-wp-nginx-ssl.sh; shift ;; #[ ]criar futuramente
         --desktop )             ./20-desktop.sh; shift ;; #[ ]criar futuramente
         --wsl )                 ./30-wsl.sh; shift ;;
+        --oci )                 ./86-oci-cli.sh; shift ;;
         --terraform )           ./87-terraform.sh; shift ;;
         --heroku-cli )          ./88-heroku-cli.sh; shift ;;
         --aws-cli )             ./89-aws-cli.sh; shift ;;
